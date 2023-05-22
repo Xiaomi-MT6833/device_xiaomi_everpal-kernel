03bb02da57d04016a57160c3ce1d31711897e480
Sushrut1101 <guptasushrut@gmail.com>
2023-05-22 18:21:23 +0530

    everpal: Import MediaTek Kernel Headers
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

---
+++ kernel-headers/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "aarch64"
+#define UTS_VERSION "#1 SMP PREEMPT Tue Jan 11 11:30:15 UTC 2022"
+#define LINUX_COMPILE_BY "build-user"
+#define LINUX_COMPILE_HOST "build-host"
+#define LINUX_COMPILER "Android (6443078 based on r383902) clang version 11.0.1 (https://android.googlesource.com/toolchain/llvm-project b397f81060ce6d701042b782172ed13bee898b79), LLD 11.0.1 (/buildbot/tmp/tmp6_m7QH b397f81060ce6d701042b782172ed13bee898b79)"
---
+++ kernel-headers/include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "4.14.186-gd695f55741f1-dirty"
